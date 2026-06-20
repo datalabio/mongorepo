7dc79d8a35b06344bd0b162eb8d4ace18a5136be
crashamol <crashamol@gmail.com>
2026-06-20 13:01:10 +0530

    main: initial project creation.

---
+++ src/mongorepo/__init__.py
@@ -0,0 +1,22 @@
+"""
+Licensed under the Apache License, Version 2.0.
+You may obtain a copy of the License at:
+```
+https://www.apache.org/licenses/LICENSE-2.0
+```
+File: __init__.py
+Description:
+
+"""
+
+# All Python Built-in Imports Here.
+
+# All Custom Imports Here.
+
+# All Native Imports Here.
+
+# All Attributes or Constants Here.
+
+
+if __name__ == '__main__':
+    pass
